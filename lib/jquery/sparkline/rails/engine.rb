7ba47f45d4a6bb81e1b2c388ddb7277fac6dffbe
Adam Krasny <akrasny@vailsys.com>
2013-01-31 16:58:16 -0600

    Version checking in engine.rb now uses Gem::Version class so it doesn't fail on 3.2.10 and above.

--- lib/jquery/sparkline/rails/engine.rb
+++ lib/jquery/sparkline/rails/engine.rb
@@ -2,7 +2,7 @@ require 'rails'
 
 module Jquery
   module Sparkline
-    if defined?(::Rails) and ::Rails.version >= "3.2.2"
+    if defined?(::Rails) and Gem::Version.new(::Rails.version) >= Gem::Version.new("3.2.2")
       module Rails
         class Engine < ::Rails::Engine
         end
